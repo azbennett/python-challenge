8959a9d4bc3193abc97bc7ed0914fa61618c253b
azbennett <stephen.f.bennett@gmail.com>
2023-04-13 17:58:40 -0700

    Week 3 PyBank and PyPoll files

---
+++ PyBank/main.py
@@ -0,0 +1,72 @@
+#Steve Bennett - Week 3 Assigment
+#Python Bank Assigment
+import csv
+import os
+import sys
+
+#CSV file containing our data
+file = 'Resources/budget_data.csv'
+
+greatestinc = 0     #tracker for greatest increase in profits
+greatestdec = 0     #tracker for greatest decrease in profits
+total = 0           #tracker for total profits
+months = 0          #tracker for total months
+average = 0         #tracker for average change 
+skipfirst = 1       #lets me enter my first loop
+totalmo = 0         #used for calculating average
+difference = []     #list for storing the difference values as we go through the loops
+old = 0             #used for old value and new value for change/difference
+new = 0             #used for old value and new value for change/difference
+diff = 0            #used for old value and new value for change/difference
+
+with open(file, 'r') as csvfile:
+    budget = csv.reader(csvfile, delimiter=',')
+    
+    next(budget, None) #skips headers
+
+    #primary loop adding up total months, total value, difference/change, and greatest inc/dec
+    for row in budget: 
+        total = total + int(row[1])
+        months = months + 1
+        
+        #first loop stores the first value as old, but does not calculate differences since we have no old value to compare to
+        if skipfirst == 1: 
+            old = int(row[1])
+            skipfirst = 0
+        #all other loops we store  new vs old for change/difference
+        else:
+            new = int(row[1])
+            diff = new - old
+            difference.append(diff)
+            old = int(row[1])
+        #check for  greatest inc / dec
+        if  diff > greatestinc:
+            greatestinc = diff
+            greatestincdate = row[0]
+        if  diff < greatestdec:
+            greatestdec = diff
+            greatestdecdate = row[0]
+
+    #calculates the average change values
+    for i in difference:
+        totalmo = totalmo + i
+    average = round(totalmo / len(difference), 2)
+
+#on screen output
+print('Financial Analysis')
+print('-----------------------------------------------------')
+print(f'Total Months: {months}')
+print(f'Total: ${total}')
+print(f'Average Change: ${average}')
+print(f'Greatest Increase in Profits: {greatestincdate} (${greatestinc})')
+print(f'Greatest Decrease in Profits: {greatestdecdate} (${greatestdec})')
+
+#I will also output the same details to a text file:
+with open('analysis/output.txt', 'w') as f:
+    f.write('Financial Analysis\n')
+    f.write('-----------------------------------------------------\n')
+    f.write(f'Total Months: {months}\n')
+    f.write(f'Total: ${total}\n')
+    f.write(f'Average Change: ${average}\n')
+    f.write(f'Greatest Increase in Profits: {greatestincdate} (${greatestinc})\n')
+    f.write(f'Greatest Decrease in Profits: {greatestdecdate} (${greatestdec})')
---
+++ PyPoll/main.py
@@ -0,0 +1,65 @@
+#Steve Bennett - Week 3 Assigment
+#Python Election Assigment
+import csv
+import os
+
+#CSV file containing our data
+file = 'Resources/election_data.csv'
+
+candidate = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"] #clean list for ouput
+totalvotes = 0              #ensures we have clean start of no votes
+percent_name = [0,0,0]      #sets list size and values to zero
+percent_val = [0,0,0]       #sets list size and values to zero
+
+with open(file, 'r') as csvfile:
+    votes = csv.reader(csvfile, delimiter=',')
+    
+    next(votes, None)       #skips the header
+    
+    for row in votes:       #primary loop counting total votes + individual candidate votes
+        totalvotes += 1
+        if row[2] == candidate[0]:
+            percent_name[0] = percent_name[0] + 1
+        elif row[2] == candidate[1]:
+            percent_name[1] = percent_name[1] + 1
+        elif row[2] == candidate[2]:
+            percent_name[2] = percent_name[2] + 1
+
+#if charles > diana and charles > raymon
+if percent_name[0] > percent_name[1] and percent_name[0] > percent_name[2]:
+    winner = candidate[0]
+#if diana > charles and diana > raymon
+elif percent_name[1] > percent_name[0] and percent_name[1] > percent_name[2]:
+    winner = candidate[1]
+#raymon is default the last option
+else:
+    winner = candidate[2]
+
+#calculates the percentage of votes + formats into ##.###% value
+for i in range(3):
+    percent_val[i] = '{:.3f}'.format((percent_name[i] / totalvotes)*100)
+
+#on screen output
+print('Election Results')
+print('-----------------------------------------------------')
+print(f'Total Votes: {totalvotes}')
+print('-----------------------------------------------------')
+print(f'{candidate[0]}: {percent_val[0]}% ({percent_name[0]})')
+print(f'{candidate[1]}: {percent_val[1]}% ({percent_name[1]})')
+print(f'{candidate[2]}: {percent_val[2]}% ({percent_name[2]})')
+print('-----------------------------------------------------')
+print(f'Winner: {winner}')
+print('-----------------------------------------------------')
+
+#I will also output the same details to a text file:
+with open('analysis/output.txt', 'w') as f:
+    f.write('Election Results\n')
+    f.write('-----------------------------------------------------\n')
+    f.write(f'Total Votes: {totalvotes}\n')
+    f.write('-----------------------------------------------------\n')
+    f.write(f'{candidate[0]}: {percent_val[0]}% ({percent_name[0]})\n')
+    f.write(f'{candidate[1]}: {percent_val[1]}% ({percent_name[1]})\n')
+    f.write(f'{candidate[2]}: {percent_val[2]}% ({percent_name[2]})\n')
+    f.write('-----------------------------------------------------\n')
+    f.write(f'Winner: {winner}\n')
+    f.write('-----------------------------------------------------')

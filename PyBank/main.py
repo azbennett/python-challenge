5c6af46683e6b7673207cc5fb819349421dd0d00
azbennett <stephen.f.bennett@gmail.com>
2023-04-16 18:21:31 -0700

    Cleaned up my variable names, started using import statistics as well

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,21 +3,22 @@
 import csv
 import os
 import sys
+import statistics
 
 #CSV file containing our data
 file = os.path.join('Resources', 'budget_data.csv')
 
-greatestinc = 0     #tracker for greatest increase in profits
-greatestdec = 0     #tracker for greatest decrease in profits
-total = 0           #tracker for total profits
-months = 0          #tracker for total months
-average = 0         #tracker for average change 
-skipfirst = 1       #lets me enter my first loop
-totalmo = 0         #used for calculating average
-difference = []     #list for storing the difference values as we go through the loops
-old = 0             #used for old value and new value for change/difference
-new = 0             #used for old value and new value for change/difference
-diff = 0            #used for old value and new value for change/difference
+greatestinc = 0         #tracker for greatest increase in profits
+greatestdec = 0         #tracker for greatest decrease in profits
+totalprofitlosses = 0   #tracker for total profits
+month_counter = 0       #tracker for total months
+average = 0             #tracker for average change 
+skipfirst = 1           #lets me enter my first loop to store previousval
+totaldifferences = 0    #used for calculating average
+difference = []         #list for storing the difference values as we go through the loops
+previousval = 0         #used for old value and new value for change/difference
+currentval = 0          #used for old value and new value for change/difference
+changeval = 0           #used for old value and new value for change/difference
 
 with open(file, 'r') as csvfile:
     budget = csv.reader(csvfile, delimiter=',')
@@ -26,37 +27,37 @@ with open(file, 'r') as csvfile:
 
     #primary loop adding up total months, total value, difference/change, and greatest inc/dec
     for row in budget: 
-        total = total + int(row[1])
-        months = months + 1
+        totalprofitlosses = totalprofitlosses + int(row[1])
+        month_counter = month_counter + 1
         
         #first loop stores the first value as old, but does not calculate differences since we have no old value to compare to
         if skipfirst == 1: 
-            old = int(row[1])
+            previousval = int(row[1])
             skipfirst = 0
         #all other loops we store  new vs old for change/difference
         else:
-            new = int(row[1])
-            diff = new - old
-            difference.append(diff)
-            old = int(row[1])
+            currentval = int(row[1])
+            changeval = currentval - previousval
+            difference.append(changeval)
+            previousval = int(row[1])
         #check for  greatest inc / dec
-        if  diff > greatestinc:
-            greatestinc = diff
+        if  changeval > greatestinc:
+            greatestinc = changeval
             greatestincdate = row[0]
-        if  diff < greatestdec:
-            greatestdec = diff
+        if  changeval < greatestdec:
+            greatestdec = changeval
             greatestdecdate = row[0]
 
     #calculates the average change values
     for i in difference:
-        totalmo = totalmo + i
-    average = round(totalmo / len(difference), 2)
+        totaldifferences = totaldifferences + i
+    average = round(statistics.mean(difference),2) #total of difference[] list divided by 85 (length of the list)
 
 #on screen output
 print('Financial Analysis')
 print('-----------------------------------------------------')
-print(f'Total Months: {months}')
-print(f'Total: ${total}')
+print(f'Total Months: {month_counter}')
+print(f'Total: ${totalprofitlosses}')
 print(f'Average Change: ${average}')
 print(f'Greatest Increase in Profits: {greatestincdate} (${greatestinc})')
 print(f'Greatest Decrease in Profits: {greatestdecdate} (${greatestdec})')
@@ -66,8 +67,8 @@ outputpath = os.path.join('analysis', 'output.txt')
 with open(outputpath, 'w') as f:
     f.write('Financial Analysis\n')
     f.write('-----------------------------------------------------\n')
-    f.write(f'Total Months: {months}\n')
-    f.write(f'Total: ${total}\n')
+    f.write(f'Total Months: {month_counter}\n')
+    f.write(f'Total: ${totalprofitlosses}\n')
     f.write(f'Average Change: ${average}\n')
     f.write(f'Greatest Increase in Profits: {greatestincdate} (${greatestinc})\n')
     f.write(f'Greatest Decrease in Profits: {greatestdecdate} (${greatestdec})')
